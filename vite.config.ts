bddbcaf39216c564930ec5911407d20b3ee4aac9
Lochlan Bennett-Odlum <lochlano@amazon.com>
2024-09-26 15:28:18 +0100

    Supress more warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      onwarn: (warning, warn) => {
+        return; // Ignore warnings
+      }
+    }
+  },
+  logLevel: 'error', // Only show errors, not warnings
 })
